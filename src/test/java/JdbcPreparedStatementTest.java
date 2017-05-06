50304802eeee7b9a225ffb63c6b3d999f15ddb48
鼠笑天 <zhuchuangang20@163.com>
2017-05-06 11:00:18 +0800

    test

--- src/test/java/JdbcPreparedStatementTest.java
+++ src/test/java/JdbcPreparedStatementTest.java
@@ -11,26 +11,23 @@ public class JdbcPreparedStatementTest {
     public void test() {
         try {
             Class.forName("org.mariadb.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:8888";
+            String url = "jdbc:mysql://localhost:3306/uaa";
             String username = "root";
             String password = "123456";
             Connection con = DriverManager.getConnection(url, username, password);
-            Statement stmt = con.createStatement();
-            String sql = "SELECT * FROM oauth_client_details WHERE client_id=?";
+            String sql = "SELECT * FROM users WHERE username=?";
             PreparedStatement pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, "client");
+            pstmt.setString(1, "admin");
             ResultSet resultSet = pstmt.executeQuery();
             while (resultSet.next()) {
                 //Retrieve by column name
-                String client_id = resultSet.getString("client_id");
-                String resource_ids = resultSet.getString("resource_ids");
-                String client_secret = resultSet.getString("client_secret");
-                String scope = resultSet.getString("scope");
+                String id = resultSet.getString("id");
+                String u = resultSet.getString("username");
+                String p = resultSet.getString("password");
                 //Display values
-                System.out.print("client_id: " + client_id);
-                System.out.print(", resource_ids: " + resource_ids);
-                System.out.print(", client_secret: " + client_secret);
-                System.out.println(", scope: " + scope);
+                System.out.print("id: " + id);
+                System.out.print(", username: " + u);
+                System.out.println(", password: " + p);
             }
         } catch (Exception se) {
             se.printStackTrace();
