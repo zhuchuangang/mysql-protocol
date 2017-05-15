f3468dfff694f40688d8f770daa5d62ee4c2e495
鼠笑天 <zhuchuangang20@163.com>
2017-05-15 14:39:17 +0800

    根据报文长度读取

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ java parse mysql protocol packet
 
 reference：
 https://github.com/xiaoma20082008/mysql-protocol
+
+
+1. 根据报文类型
+mysql响应的报文类型有下面5种：
+```
+OK packet,LOCAL_INFILE packet,EOF packet,ERROR packet,ResultSet Row
+```
+除了ResultSet Row报文长度不确定之外，其他都可以通过header确定类型，确定了类型也就确定了长度。
+对于ResultSet Row报文通过报文状态获取每一块的报文类型，ResultSet Row报文大致可以分2大块，一块是列信息，一块是行信息，可做分别输出。
+
+
+2. 一个一个报文分别输出
--- src/main/java/leader/us/mysql/net/BackendHandler.java
+++ src/main/java/leader/us/mysql/net/BackendHandler.java
@@ -4,12 +4,17 @@ import leader.us.mysql.bufferpool.Chunk;
 import leader.us.mysql.bufferpool.DirectByteBufferPool;
 import leader.us.mysql.protocol.packet.HandshakePacket;
 import leader.us.mysql.protocol.packet.StmtPrepareOKPacket;
+import leader.us.mysql.protocol.support.BufferUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by zcg on 2017/5/2.
@@ -45,25 +50,90 @@ public class BackendHandler extends NioHandler {
         writeData(chunk);
     }
 
+//    @Override
+//    public void doReadData() throws IOException {
+//        Chunk chunk = bufferPool.getChunk(1024);
+//        int readNum = this.connection.getSocketChannel().read(chunk.getBuffer());
+//        chunk.getBuffer().flip();
+//
+//        if (readNum == 0) {
+//            return;
+//        }
+//        if (readNum == -1) {
+//            this.connection.getSocketChannel().socket().close();
+//            this.connection.getSocketChannel().close();
+//            selectionKey.cancel();
+//            return;
+//        }
+//
+//        if (frontendHandler != null) {
+//            MysqlResponseHandler.dump(chunk.getBuffer(), frontendHandler);
+//            frontendHandler.writeData(chunk);
+//        }
+//    }
+
+    private volatile int packetLength = 0;
+    private byte[] lengthByte;
+    private AtomicBoolean reading = new AtomicBoolean(false);
+
     @Override
     public void doReadData() throws IOException {
-        Chunk chunk = bufferPool.getChunk(1024);
-        int readNum = this.connection.getSocketChannel().read(chunk.getBuffer());
-        chunk.getBuffer().flip();
+        while (!reading.compareAndSet(false, true)) {
 
-        if (readNum == 0) {
-            return;
         }
-        if (readNum == -1) {
-            this.connection.getSocketChannel().socket().close();
-            this.connection.getSocketChannel().close();
-            selectionKey.cancel();
-            return;
-        }
+        try {
+            SocketChannel channel = this.connection.getSocketChannel();
+            lengthByte = new byte[3];
+            ByteBuffer b = ByteBuffer.wrap(lengthByte);
+            if (packetLength == 0) {
+                int readNum = channel.read(b);
+                logger.debug("1.read packet length byte is "+readNum);
+                if (readNum == 0) {
+                    reading.lazySet(false);
+                    return;
+                }
+                if (readNum == -1) {
+                    this.connection.getSocketChannel().socket().close();
+                    this.connection.getSocketChannel().close();
+                    selectionKey.cancel();
+                    reading.lazySet(false);
+                    return;
+                }
+                int length = lengthByte[0] & 0xff;
+                length |= (lengthByte[1] & 0xff) << 8;
+                length |= (lengthByte[2] & 0xff) << 16;
+                packetLength = length;
+                logger.debug("2.read packet length is "+packetLength);
+                reading.lazySet(false);
+                return;
+            } else {
+                logger.debug("3.packetLength is "+packetLength);
+                Chunk chunk = bufferPool.getChunk(packetLength + 4);
+                BufferUtil.writeUB3(chunk.getBuffer(), packetLength);
+                packetLength = 0;
 
-        if (frontendHandler != null) {
-            MysqlResponseHandler.dump(chunk.getBuffer(), frontendHandler);
-            frontendHandler.writeData(chunk);
+                int readNum = channel.read(chunk.getBuffer());
+                logger.debug("4.packetLength is "+packetLength+",read packet body is "+readNum+",chuck buffer size is "+chunk.getBuffer().capacity());
+                if (readNum == 0) {
+                    reading.lazySet(false);
+                    return;
+                }
+                if (readNum == -1) {
+                    this.connection.getSocketChannel().socket().close();
+                    this.connection.getSocketChannel().close();
+                    selectionKey.cancel();
+                    reading.lazySet(false);
+                    return;
+                }
+
+                chunk.getBuffer().flip();
+                if (frontendHandler != null) {
+                    //MysqlResponseHandler.dump(chunk.getBuffer(), frontendHandler);
+                    frontendHandler.writeData(chunk);
+                }
+            }
+        } finally {
+            reading.lazySet(false);
         }
     }
 
